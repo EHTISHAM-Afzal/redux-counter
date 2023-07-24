82591ea9c9cdd1dd5c51810aa3ac959d077100b8
sham <khaj3172@gmail.com>
2023-07-24 21:01:35 +0500

    modified:   src/features/Counter.jsx

--- src/features/Counter.jsx
+++ src/features/Counter.jsx
@@ -38,7 +38,7 @@ const Counter = () => {
         >
           +
         </Button>
-        <Typography variant="h1" color="initial" sx={{  maxWidth: "50vw" , overflow: "auto" }}>
+        <Typography variant="h1" color="initial" sx={{  maxWidth: "70vw" , overflow: "auto" }}>
           {count}
         </Typography>
         <Button
